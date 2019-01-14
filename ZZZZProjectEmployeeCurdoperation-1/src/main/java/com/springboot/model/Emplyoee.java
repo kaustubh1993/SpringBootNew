794da4f74438a760ccf958e1910ebbad172e62cd
kaustubh1993 <kaustubhwarokar@gmail.com>
2019-01-14 23:32:45 +0530

    Lambok

--- ZZZZProjectEmployeeCurdoperation-1/src/main/java/com/springboot/model/Emplyoee.java
+++ ZZZZProjectEmployeeCurdoperation-1/src/main/java/com/springboot/model/Emplyoee.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "Dev.Employee")
 public class Emplyoee {
@@ -35,124 +38,5 @@ public class Emplyoee {
 	private Long pincode;
 	@Column(name="deptno")
 	private Integer deptno;
-
-	// constructor and default constructor
-	public Emplyoee() {
-	}
-
-	public Emplyoee(Long id, String firstName, String lastName, String middleName, String email, Long mobileNumber,
-			String address1, String address2,  String state, Long pincode, Integer deptno) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.middleName = middleName;
-		this.email = email;
-		this.mobileNumber = mobileNumber;
-		this.address1 = address1;
-		this.address2 = address2;
-		this.state = state;
-		this.pincode = pincode;
-		this.deptno = deptno;
-	}
-
-	// getter and setter start
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getMiddleName() {
-		return middleName;
-	}
-
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Long getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(Long mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-
-	public String getAddress1() {
-		return address1;
-	}
-
-	public void setAddress1(String address1) {
-		this.address1 = address1;
-	}
-
-	public String getAddress2() {
-		return address2;
-	}
-
-	public void setAddress2(String address2) {
-		this.address2 = address2;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public Long getPincode() {
-		return pincode;
-	}
-
-	public void setPincode(Long pincode) {
-		this.pincode = pincode;
-	}
-
-	public Integer getDeptno() {
-		return deptno;
-	}
-
-	public void setDeptno(Integer deptno) {
-		this.deptno = deptno;
-	}
-
-	// getter and setter end
-
-	// toStringmethod
-	@Override
-	public String toString() {
-		return "Emplyoee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", middleName="
-				+ middleName + ", email=" + email + ", mobileNumber=" + mobileNumber + ", address1=" + address1
-				+ ", address2=" + address2 + ",  state=" + state + ", pincode=" + pincode
-				+ ", deptno=" + deptno + "]";
-	}
 
 }
